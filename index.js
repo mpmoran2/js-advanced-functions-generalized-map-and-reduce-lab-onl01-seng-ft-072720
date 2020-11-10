310bde6f51da73bedf8f661acf7a97e2b751120f
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-11-10 18:02:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 // Add your functions here
 
 //map
+const map = function(array, callback){
+    const newArray = []
+    for (let i = 0; i < array.length; i++){
+    newArray.push(callback(array[i]))
+}
+    return newArray
+};
 
 //reduce
