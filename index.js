9c64e2777f982212be93eeea3cb2898423f22055
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-11-10 18:04:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions here
+
+//map
+const map = function(array, callback){
+    const newArray = []
+    for (let i = 0; i < array.length; i++){
+    newArray.push(callback(array[i]))
+}
+    return newArray
+};
+
+//reduce
+const reduce = function(src, cb, starting){
+    let r = (!!starting) ? starting : src[0]
+    let i = (!!starting) ? 0 : 1
+    for (; i < src.length; i++){
+        r = cb(src[i], r)
+    }
+    return r;
+};
